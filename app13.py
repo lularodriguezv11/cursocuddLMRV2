856ae919068c90bbbce352185c1913a681b1e1ea
lularodriguezv11 <lmrodrig@uach.mx>
2025-05-28 17:59:35 -0600

    Update app13.py

--- app13.py
+++ app13.py
@@ -3,10 +3,10 @@ import streamlit as st
 
 st.set_page_config(page_title="Ejemplo Chat", layout="centered")
 
-st.title("💬 Mini Chatbot (solo repite lo que dices)")
+st.title("💬 Mini Chatbot Simón dice!")
 
 # Entrada tipo chat (abajo de la pantalla)
-user_input = st.chat_input("Escribe algo...")
+user_input = st.chat_input("Tú primero!...")
 
 # Si el usuario escribe algo, mostramos los mensajes
 if user_input:
