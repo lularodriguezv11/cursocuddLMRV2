828f3af730bdbdd3e8af52f808b5f184ea482250
lularodriguezv11 <lmrodrig@uach.mx>
2025-05-30 16:04:05 -0600

    Update app16.py

--- app16.py
+++ app16.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-# Show title and description.
+ Show title and description.
 st.title("💬 Chatbot")
 
 openai_api_key = st.secrets["api_key"] 
@@ -12,21 +12,21 @@ prompt = st.chat_input("What is up?")
 if prompt==None:
    st.stop()
 
-#with st.chat_message("user"):
-#   st.markdown(prompt)
+with st.chat_message("user"):
+   st.markdown(prompt)
 
 # Generate a response using the OpenAI API.
 
-#stream = client.chat.completions.create(
-#        model="gpt-4o-mini",  
-#        messages=[
-#            {"role": "system", "content": "You are an assistant."},
-#            {"role": "user", "content": prompt}
-#        ],
-#        max_tokens=800,
-#        temperature=0,
-#    )
-#respuesta = stream.choices[0].message.content
+stream = client.chat.completions.create(
+        model="gpt-4o-mini",  
+        messages=[
+            {"role": "system", "content": "You are an assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=800,
+        temperature=0,
+    )
+respuesta = stream.choices[0].message.content
 
-#with st.chat_message("assistant"):
-#   st.write(respuesta)
+with st.chat_message("assistant"):
+   st.write(respuesta)
