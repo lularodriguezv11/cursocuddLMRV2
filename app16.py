6b160129ead3047be8988aaff8964b7f8bd02cd8
lularodriguezv11 <lmrodrig@uach.mx>
2025-05-30 16:02:10 -0600

    Update app16.py

--- app16.py
+++ app16.py
@@ -8,9 +8,9 @@ openai_api_key = st.secrets["api_key"]
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
 
-#prompt = st.chat_input("What is up?")
-#if prompt==None:
-#   st.stop()
+prompt = st.chat_input("What is up?")
+if prompt==None:
+   st.stop()
 
 #with st.chat_message("user"):
 #   st.markdown(prompt)
